510ed33287c167c8765737e829da5605f20a4c47
Nishant Gupta <shub002gupta@gmail.com>
2023-03-19 03:08:51 +0530

    Module testing file

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from src.configuration.mongodb_connection import MongoDatabaseClient
+from src.exception import SensorException
+from src.logger import logging
+import sys
 
+# Test Exceptions
+def test_exception():
+    try:
+        logging.info("Test exception")
+        x=1/0
+    except Exception as e:
+        raise SensorException(e,sys) # type: ignore
 if __name__ == '__main__':
-    mongo_db_client = MongoDatabaseClient()
-    print(mongo_db_client.database.list_collection_names()) # type: ignore
+    # mongo_db_client = MongoDatabaseClient()
+    # print(mongo_db_client.database.list_collection_names()) # type: ignore
+    try:
+        test_exception()
+    except Exception as e:
+        print(e)
